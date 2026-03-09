bbd3a64448f8b5b0bfbdc59cceebbf60a63fae69
Zanno <975599731@qq.com>
2026-03-09 15:16:52 +0800

    环境配置以及MD5加密

--- sky-common/src/main/java/com/sky/properties/WeChatProperties.java
+++ sky-common/src/main/java/com/sky/properties/WeChatProperties.java
@@ -1,13 +1,14 @@
 package com.sky.properties;
 
-import lombok.Data;
+import lombok.*;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 @Component
 @ConfigurationProperties(prefix = "sky.wechat")
-@Data
+@Getter
+@Setter
 public class WeChatProperties {
 
     private String appid; //小程序的appid
@@ -19,5 +20,76 @@ public class WeChatProperties {
     private String weChatPayCertFilePath; //平台证书
     private String notifyUrl; //支付成功的回调地址
     private String refundNotifyUrl; //退款成功的回调地址
-
+//
+//    public String getAppid() {
+//        return appid;
+//    }
+//
+//    public void setAppid(String appid) {
+//        this.appid = appid;
+//    }
+//
+//    public String getSecret() {
+//        return secret;
+//    }
+//
+//    public void setSecret(String secret) {
+//        this.secret = secret;
+//    }
+//
+//    public String getMchid() {
+//        return mchid;
+//    }
+//
+//    public void setMchid(String mchid) {
+//        this.mchid = mchid;
+//    }
+//
+//    public String getMchSerialNo() {
+//        return mchSerialNo;
+//    }
+//
+//    public void setMchSerialNo(String mchSerialNo) {
+//        this.mchSerialNo = mchSerialNo;
+//    }
+//
+//    public String getPrivateKeyFilePath() {
+//        return privateKeyFilePath;
+//    }
+//
+//    public void setPrivateKeyFilePath(String privateKeyFilePath) {
+//        this.privateKeyFilePath = privateKeyFilePath;
+//    }
+//
+//    public String getApiV3Key() {
+//        return apiV3Key;
+//    }
+//
+//    public void setApiV3Key(String apiV3Key) {
+//        this.apiV3Key = apiV3Key;
+//    }
+//
+//    public String getWeChatPayCertFilePath() {
+//        return weChatPayCertFilePath;
+//    }
+//
+//    public void setWeChatPayCertFilePath(String weChatPayCertFilePath) {
+//        this.weChatPayCertFilePath = weChatPayCertFilePath;
+//    }
+//
+//    public String getNotifyUrl() {
+//        return notifyUrl;
+//    }
+//
+//    public void setNotifyUrl(String notifyUrl) {
+//        this.notifyUrl = notifyUrl;
+//    }
+//
+//    public String getRefundNotifyUrl() {
+//        return refundNotifyUrl;
+//    }
+//
+//    public void setRefundNotifyUrl(String refundNotifyUrl) {
+//        this.refundNotifyUrl = refundNotifyUrl;
+//    }
 }
